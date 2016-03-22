c6132e1d5bde706228c22034061de061a5cc1033
michgerts <michgerts@gmail.com>
2016-03-22 07:08:51 +0200

    Initial html and css done
    Initial design is done js work begins

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 /**
- * JS Library v0
+ * Netcraft webapp main JS file
  */
 
-var UTILS = (function () {
-
-	return {
-		/**
-		 * Check if a given value is a plain Object
-		 *
-		 * @param  {*}       o Any value to be checked
-		 * @return {Boolean}   true if it's an Object
-		 */
-		isObject: function (o) {
-			var toString = Object.prototype.toString;
-			return (toString.call(o) === toString.call({}));
-		}
-    
-	};
-}());
