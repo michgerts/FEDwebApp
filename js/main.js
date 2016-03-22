a04bf6ac989c9bd7c904a19f7689e0c6b2dc2d19
michgerts <michgerts@gmail.com>
2016-03-22 09:13:31 +0200

    show active tab and hide others

--- js/main.js
+++ js/main.js
@@ -2,3 +2,45 @@
  * Netcraft webapp main JS file
  */
 
+var tabNames = ['#quick-reports', '#fmy-folders', '#my-team-folders', '#public-folders'];
+
+/* load the correct tab upon webpage load */
+function initActiveTab ()
+{
+	/* get the hash addresss from the url */
+	var winLocation = window.location.href;
+	var activeTab = '#'+winLocation.split('#')[1];
+	if ((tabNames.indexOf(activeTab) == -1) || (activeTab==undefined) )
+	{
+		$('#tabList a[href="#quick-reports"]').trigger('click');
+	}
+	else
+	{
+		$('#tabList a[href="'+activeTab+'"]').trigger('click');
+	}
+}
+
+/** 
+ * load the correct tab upon click on a link:
+ * see $(document).on('click','#tabList a',setActiveTab) for call fir function
+ */
+function setActiveTab ()
+{
+	$('#tabList > li').removeClass('activeTab');
+	$(this).parent().addClass('activeTab');
+	/* hide all tabs and show the "correct" one */
+	$.each(tabNames, function (index, value)
+	{
+		$(value).addClass('hidden');
+	});
+	$($(this).attr('href')).removeClass('hidden');
+}
+
+$(document).on('click','#tabList a',setActiveTab);
+/**
+ * Function to be prefromed after the document is ready
+ */
+$(document).ready(function()
+{
+	initActiveTab();
+});
